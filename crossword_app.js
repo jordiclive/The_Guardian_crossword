1ce29ab9e4de1f3c6ef0346c766b88d5f9a67843
jordiclive <44066010+jordiclive@users.noreply.github.com>
2019-01-08 12:19:58 +0000

    Update crossword_app.js

--- crossword_app.js
+++ crossword_app.js
@@ -5,15 +5,15 @@
 
 
 var today = new Date();
-var reference_date = new Date("6/5/2018");
+var reference_date = new Date("1/2/2019");
 var time_difference = Math.abs(today.getTime() - reference_date.getTime());
 var day_difference = Math.ceil(time_difference / (1000 * 3600 * 24))-1;
 
 
 if ((day_difference % 7) == 0) {
-    x = Math.floor(day_difference/7 + 1191);
+    x = Math.floor(day_difference/7 + 1213);
     window.open('https://www.theguardian.com/crosswords/speedy/'+String(x));
 } else {
-    x = Math.floor(day_difference%7 + ((day_difference - day_difference%7) / 7.0)*6 + 15000);
+    x = Math.floor(day_difference%7 + ((day_difference - day_difference%7) / 7.0)*6 + 15179);
     window.open('https://www.theguardian.com/crosswords/quick/'+String(x));
 }
